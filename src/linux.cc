ec722cb81ca0ca4aad58a31c7dcd6f64ee53ab0c
Caio Freitas de Oliveira <caio.freitaso@gmail.com>
2022-10-04 21:07:01 -0300

    reduce sleep time for cpu_usage

--- src/linux.cc
+++ src/linux.cc
@@ -1086,7 +1086,7 @@ int update_running_processes(void) {
 }
 
 int update_cpu_usage(void) {
-  struct timespec tc = {0L, 100L * 1000000L};
+  struct timespec tc = {0L, 1000000L};
   update_stat();
   if (-1 == (nanosleep(&tc, NULL))) {
     NORM_ERR("update_cpu_usage(): nanosleep() failed");
